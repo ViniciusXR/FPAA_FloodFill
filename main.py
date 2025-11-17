7c6ef7c9d7d840b2dbbff8f213f54505a523b823
Vinicius Xavier <viniciusxr89@gmail.com>
2025-11-17 12:06:45 -0300

    Add initial implementation of Flood Fill algorithm in Python

---
+++ main.py
@@ -0,0 +1,5 @@
+''' Implementação em Python do algoritmo Flood Fill para identificar e preencher automaticamente todas as regiões conectadas em um grid 2D, 
+utilizando diferentes cores para cada área.'''
+
+if __name__ == "__main__":
+	pass
